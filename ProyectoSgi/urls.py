ad2df16138939945eca12b6f59f13ad3ac682882
cristianSanta <cristiansanta5@gmail.com>
2023-09-27 17:37:08 -0500

    cambio talhia

--- ProyectoSgi/urls.py
+++ ProyectoSgi/urls.py
@@ -26,6 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login_view, name='login_view'),
     path('base/', views.baseAdmin_view, name='baseAdmin_view'),
+    path('regUsuario/', views.registroUsuario_view, name='registroUsuario_view'),
+    path('formPrestamos/', views.formPrestamos_view, name='formPrestamos_view'),
+    path('formElementos/', views.formElementos_view, name='formElementos_view'),
     
 ]
 
