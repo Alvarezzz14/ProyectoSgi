01d7774e3fa5bd8e5b2e994e71636dcedf036469
cristianSanta <cristiansanta5@gmail.com>
2023-09-27 17:02:15 -0500

    modified:   UsuariosSena/templates/superAdmin/basesuadmin.html 	modified:   UsuariosSena/templates/superAdmin/formElementos.html 	modified:   UsuariosSena/templates/superAdmin/registroUsuario.html 	modified:   UsuariosSena/views.py

--- UsuariosSena/views.py
+++ UsuariosSena/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def login_view(request):
+    return render(request, 'indexLogin.html')
+
 def baseAdmin_view(request):
     return render(request, 'superAdmin/basesuadmin.html')
 
-def login_view(request):
-    return render(request, 'indexLogin.html')
+def registroUsuario_view(request):
+    return render(request, 'superAdmin/registroUsuario.html')
+
+def formPrestamos_view(request):
+    return render(request, 'superAdmin/formPrestamos.html')
+
+def formElementos_view(request):
+    return render(request, 'superAdmin/formElementos.html')
