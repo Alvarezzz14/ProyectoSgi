af60aca5017fc911da187b6bf8a5e6c58bc60fcb
cristianSanta <cristiansanta5@gmail.com>
2023-09-27 19:32:57 -0500

    modified:   ProyectoSgi/__pycache__/__init__.cpython-311.pyc 	modified:   ProyectoSgi/__pycache__/settings.cpython-311.pyc 	modified:   ProyectoSgi/__pycache__/urls.cpython-311.pyc 	modified:   UsuariosSena/__pycache__/__init__.cpython-311.pyc 	modified:   UsuariosSena/__pycache__/admin.cpython-311.pyc 	modified:   UsuariosSena/__pycache__/choices.cpython-311.pyc 	new file:   UsuariosSena/__pycache__/forms.cpython-311.pyc 	modified:   UsuariosSena/__pycache__/models.cpython-311.pyc 	modified:   UsuariosSena/__pycache__/views.cpython-311.pyc 	modified:   UsuariosSena/forms.py 	modified:   UsuariosSena/templates/registration/login.html 	modified:   UsuariosSena/views.py

--- UsuariosSena/views.py
+++ UsuariosSena/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import LoginForm
 
 # Create your views here.
 
@@ -17,3 +18,18 @@ def formPrestamos_view(request):
 def formElementos_view(request):
     return render(request, 'superAdmin/formElementos.html')
 
+
+#prueba de loginnnnnnnnnnn
+def login_view(request):
+    if request.method == 'POST':
+        # Procesa el formulario si se envió
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # Aquí puedes realizar acciones adicionales, como la autenticación
+            # Si el formulario es válido, redirige al usuario a la página de inicio o a donde sea necesario.
+            return HttpResponseRedirect('/inicio/')
+    else:
+        # Renderiza un formulario vacío si no se envió uno
+        form = LoginForm()
+
+    return render(request, 'registration/login.html', {'form': form})
